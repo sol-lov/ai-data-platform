f69129a383a4f2d8f1a99ce3157c752435ab772a
Your Name <you@example.com>
2026-02-27 00:26:09 +0330

    ts

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// File: prisma.config.ts
+
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url: env('DATABASE_URL_UNPOOLED'),
+  },
+  migrations: {
+    path: 'prisma/migrations',
+  },
+});
