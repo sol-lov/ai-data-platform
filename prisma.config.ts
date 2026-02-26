1f2e2226acb38e29785dfe0b311f5acc569708c5
Your Name <you@example.com>
2026-02-27 00:28:35 +0330

    url: env('DATABASE_URL'),

--- prisma.config.ts
+++ prisma.config.ts
@@ -6,7 +6,7 @@ import { defineConfig, env } from 'prisma/config';
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL_UNPOOLED'),
+    url: env('DATABASE_URL'),
   },
   migrations: {
     path: 'prisma/migrations',
